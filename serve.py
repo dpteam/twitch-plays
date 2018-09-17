de170925628a819578833489dfd793342bb6aaaf
Joaquin <18078964+Jacknet@users.noreply.github.com>
2018-09-17 11:11:46 -0400

    Update serve.py

--- serve.py
+++ serve.py
@@ -7,6 +7,8 @@ import lib.bot as bot
 # Twitch Plays
 # Inpsired by http://twitch.tv/twitchplayspokemon
 # Written by Aidan Thomson - <aidraj0 at gmail dot com>
+# Button modifications for SNES/DS use by JackNet
+# *nix implementation by ynohtna92
 
 try:
     bot.Bot().run()
